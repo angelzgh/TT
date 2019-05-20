1877cac7c0860e2c8d09a098c65a62f97c57e885
Angel <angel@10.100.12.205>
2019-05-20 04:34:20 -0500

    Merge origin/master
    Conflicts:
    	src/main/java/com/ipn/mx/tt/controller/TestEspecialistaController.java
    	src/main/java/com/ipn/mx/tt/controller/TestEspecialistaFinalizadoController.java
    	src/main/java/com/ipn/mx/tt/controller/TestPacientePreguntasController.java

--- src/com/ipn/mx/tt/controller/RecomendacionesController.java
+++ src/com/ipn/mx/tt/controller/RecomendacionesController.java
@@ -272,8 +272,12 @@ public class RecomendacionesController implements Initializable {
         parametros.put("curp", paciente.getCURP());
         parametros.put("escolaridad", paciente.getEscolaridad());
         parametros.put("recoe", "Texto de prueba");
-            parametros.put("recos", "1");
-     
+        parametros.put("recos", "Prueba");
+            if(resultado.getT1()==1.0){
+            parametros.put("insomnio","Si");
+            }else{
+            parametros.put("insomnio","No");
+            }
         JasperPrint informe = JasperFillManager.fillReport(master, parametros, new JREmptyDataSource());
 //JasperViewer.viewReport(informe,false);
         JasperExportManager.exportReportToPdfFile(informe, "C://TT//" + paciente.getNombre() + ".pdf");
