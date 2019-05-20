ee8278641cb6278f1da854f4b09f6a6a90ece548
Axel Reyes <34036239+Tuky88@users.noreply.github.com>
2019-05-20 04:31:07 -0500

    mejoras

--- src/com/ipn/mx/tt/dao/ResultadoDAO.java
+++ src/com/ipn/mx/tt/dao/ResultadoDAO.java
@@ -5,24 +5,43 @@
  */
 package com.ipn.mx.tt.dao;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
+import java.util.List;
 
 /**
  *
  * @author Axel Reyes
  */
-public class ResultadoDAO extends DocumentoDAO{
-    
+public class ResultadoDAO extends DocumentoDAO {
+
     public ResultadoDAO(String base, String coleccion) {
         super(base, coleccion);
     }
-    public ResultadoDAO()
-    {
+
+    public ResultadoDAO() {
         super("TT", "Resultado");
     }
-    
-    public void insertarResultado(DBObject dbo)
-    {
+
+    public void insertarResultado(DBObject dbo) {
         cjm.getMongoCollection().insert(dbo);
+    }
+
+    public List obtenerTodo() {
+        DBCursor cursor = cjm.getMongoCollection().find();
+        return cursor.toArray();
+    }
+
+    public List obtenerHombre() {
+        DBObject dbo = new BasicDBObject("sexo", "H");
+        DBCursor cursor = cjm.getMongoCollection().find();
+        return cursor.toArray();
+    }
+
+    public List obtenerMujer() {
+        DBObject dbo = new BasicDBObject("sexo", "M");
+        DBCursor cursor = cjm.getMongoCollection().find();
+        return cursor.toArray();
     }
 }
